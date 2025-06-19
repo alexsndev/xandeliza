288c73135f405624cd5b5e4437d86f76ca15fe8b
alexnsdev <alexandrepessoalrodrigues@gmail.com>
2025-06-18 21:04:13 -0300

    Corrige rota duplicada admin.banners.update

--- routes/web.php
+++ routes/web.php
@@ -35,15 +35,15 @@ Route::middleware('auth')->group(function () {
     });
 
     // Rotas de Banners
-    Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
-        Route::get('/banners', [BannerController::class, 'index'])->name('banners.index');
-        Route::get('/banners/create', [BannerController::class, 'create'])->name('banners.create');
-        Route::post('/banners', [BannerController::class, 'store'])->name('banners.store');
-        Route::get('/banners/{banner}/edit', [BannerController::class, 'edit'])->name('banners.edit');
-        Route::put('/banners/{banner}', [BannerController::class, 'update'])->name('banners.update');
-        Route::delete('/banners/{banner}', [BannerController::class, 'destroy'])->name('banners.destroy');
-        Route::post('/banners/{banner}/toggle', [BannerController::class, 'toggleStatus'])->name('banners.toggle');
-    });
+    // Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    //     Route::get('/banners', [BannerController::class, 'index'])->name('banners.index');
+    //     Route::get('/banners/create', [BannerController::class, 'create'])->name('banners.create');
+    //     Route::post('/banners', [BannerController::class, 'store'])->name('banners.store');
+    //     Route::get('/banners/{banner}/edit', [BannerController::class, 'edit'])->name('banners.edit');
+    //     Route::put('/banners/{banner}', [BannerController::class, 'update'])->name('banners.update');
+    //     Route::delete('/banners/{banner}', [BannerController::class, 'destroy'])->name('banners.destroy');
+    //     Route::post('/banners/{banner}/toggle', [BannerController::class, 'toggleStatus'])->name('banners.toggle');
+    // });
 });
 
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
